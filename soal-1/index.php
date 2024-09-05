646d08a49acb7227650ccd72e7bcc2d3f1eefcb4
ItsMeAGS <160562717+ANANDAGUSTI@users.noreply.github.com>
2024-09-05 10:14:52 +0800

    Add files via upload

---
+++ soal-1/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Soal 1</title>
+</head>
+<body>
+    <h1>Soal 1: Hitung Volume Tabung</h1>
+    <form action="volume.php" method="post">
+        <label for="radius">Jari-jari alas (r):</label>
+        <input type="number" id="radius" name="radius" step="any" required><br><br>
+
+        <label for="height">Tinggi Tabung (h):</label>
+        <input type="number" id="height" name="height" step="any" required><br><br>
+
+        <input type="submit" value="Hitung Volume">
+    </form>
+</body>
+</html>
